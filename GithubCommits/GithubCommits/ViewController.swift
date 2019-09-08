960374bb6111ef9accad87881ae9733d2b236423
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2019-09-08 15:48:30 -0500

    Create a project
    This is iOS Swift project

---
+++ GithubCommits/GithubCommits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubCommits
+//
+//  Created by Soriyany keo on 9/8/19.
+//  Copyright © 2019 Soriyany keo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
