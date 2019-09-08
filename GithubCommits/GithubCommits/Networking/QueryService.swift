f838e5521d853218fe73b22381fb09044fe04c82
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2019-09-08 16:34:35 -0500

    Implement a method to retrieve commit data from Github API

--- GithubCommits/GithubCommits/Networking/QueryService.swift
+++ GithubCommits/GithubCommits/Networking/QueryService.swift
@@ -13,4 +13,59 @@ import Foundation
 
 /// Runs query data task, and stores results in array of Responses
 class QueryService {
+    //
+    // MARK: - Constants
+    //
+    let defaultSession = URLSession(configuration: .default)
+    
+    //
+    // MARK: - Variables And Properties
+    //
+    var dataTask: URLSessionDataTask?
+    var errorMessage = ""
+    var responses: [Response] = []
+    
+    //
+    // MARK: - Type Alias
+    //
+    typealias JSONDictionary = [String: Any]
+    typealias QueryResult = ([Response]?, String) -> Void
+    
+    //
+    // MARK: - Internal Methods
+    //
+    
+    func getResults(searchTerm: String, completion: @escaping QueryResult) {
+        // 1
+        dataTask?.cancel()
+        
+        // 2
+        if var urlComponents = URLComponents(string: "https://api.github.com/repos/soke1/GithubCommits/commits") {
+            
+            // 3
+            guard let url = urlComponents.url else {
+                return
+            }
+            
+            // 4
+            dataTask = defaultSession.dataTask(with: url) { [weak self] data, response, error in
+                defer {
+                    self?.dataTask = nil
+                }
+                
+                // 5
+                if let error = error {
+                    self?.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
+                } else if
+                    let data = data,
+                    let response = response as? HTTPURLResponse,
+                    response.statusCode == 200 {
+                    print(data)
+                }
+            }
+            
+            // 7
+            dataTask?.resume()
+        }
+    }
 }
