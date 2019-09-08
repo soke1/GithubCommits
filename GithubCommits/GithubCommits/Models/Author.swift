2b9faa2fc995925bdbcceaf58b8f0d83363d28e5
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2019-09-08 16:25:24 -0500

    Create Author struct as model data for encoding json

---
+++ GithubCommits/GithubCommits/Models/Author.swift
@@ -0,0 +1,12 @@
+//
+//  Response.swift
+//  GithubCommits
+//
+//  Created by Soriyany keo on 9/8/19.
+//  Copyright © 2019 Soriyany keo. All rights reserved.
+//
+
+import Foundation
+struct Author: Codable { // or Decodable
+    
+}
