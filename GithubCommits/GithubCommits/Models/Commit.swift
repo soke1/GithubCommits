45384f297f648f773518b32a35e301a6a206476e
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2019-09-08 16:26:56 -0500

    Create Commit struct as model data for encoding json

---
+++ GithubCommits/GithubCommits/Models/Commit.swift
@@ -0,0 +1,11 @@
+//
+//  Commit.swift
+//  GithubCommits
+//
+//  Created by Soriyany keo on 9/8/19.
+//  Copyright © 2019 Soriyany keo. All rights reserved.
+//
+
+import Foundation
+struct Commit: Codable { // or Decodable
+}
