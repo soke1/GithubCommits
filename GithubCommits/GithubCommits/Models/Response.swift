9aa3df555f04e24ba69e0ad4868ceca0f3ca1e7b
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2019-09-08 16:28:18 -0500

    Create Response struct as model data for encoding json

---
+++ GithubCommits/GithubCommits/Models/Response.swift
@@ -0,0 +1,11 @@
+//
+//  Response.swift
+//  GithubCommits
+//
+//  Created by Soriyany keo on 9/8/19.
+//  Copyright © 2019 Soriyany keo. All rights reserved.
+//
+
+import Foundation
+struct Response: Codable { // or Decodable
+}
