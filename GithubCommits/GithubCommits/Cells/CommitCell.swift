7e30846cf860a42ffec1d8ab06bf2efc1ac43ff5
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2019-09-08 16:49:51 -0500

    Add func to setup commit data to cell properties

--- GithubCommits/GithubCommits/Cells/CommitCell.swift
+++ GithubCommits/GithubCommits/Cells/CommitCell.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 class CommitCell: UITableViewCell {
+    //
+    // MARK: - Class Constants
+    //
+    static let identifier = "CommitCell"
+    
+    //
+    // MARK: - IBOutlets
+    //
     @IBOutlet weak var authorNameLbl: UILabel!
     @IBOutlet weak var shaLbl: UILabel!
     @IBOutlet weak var messageLbl: UILabel!
+    
+    func setup(response:Response){
+        authorNameLbl.text = response.commit.author.name
+        shaLbl.text = response.sha
+        messageLbl.text = response.commit.message
+    }
 }
