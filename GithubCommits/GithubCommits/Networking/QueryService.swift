f27602cf53026fc8616621ccb4ad4ed6a95f4493
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2019-09-08 16:30:55 -0500

    Create QueryService to run data task and store results in array of Responses

---
+++ GithubCommits/GithubCommits/Networking/QueryService.swift
@@ -0,0 +1,16 @@
+//
+//  QueryService.swift
+//  GithubCommits
+//
+//  Created by Soriyany keo on 9/8/19.
+//  Copyright © 2019 Soriyany keo. All rights reserved.
+//
+
+import Foundation
+//
+// MARK: - Query Service
+//
+
+/// Runs query data task, and stores results in array of Responses
+class QueryService {
+}
