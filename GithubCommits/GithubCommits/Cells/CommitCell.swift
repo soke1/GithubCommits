521d4626c44544b42f65e20d7aa1945dc5a0abd5
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2019-09-08 16:16:12 -0500

    Create a subclass for table view cell

---
+++ GithubCommits/GithubCommits/Cells/CommitCell.swift
@@ -0,0 +1,11 @@
+//
+//  CommitCell.swift
+//  GithubCommits
+//
+//  Created by Soriyany keo on 9/8/19.
+//  Copyright © 2019 Soriyany keo. All rights reserved.
+//
+
+import UIKit
+class CommitCell: UITableViewCell {
+}
