a60cdfa1b382b3a80d88b18cca8d9eaccf86b487
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2019-09-08 16:39:35 -0500

    Connect tableView IBOutlet from storyboard to ViewController class

--- GithubCommits/GithubCommits/ViewControllers/ViewController.swift
+++ GithubCommits/GithubCommits/ViewControllers/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
